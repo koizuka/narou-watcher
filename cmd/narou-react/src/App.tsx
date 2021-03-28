486b73005f8bbb33fc0ee57aee88c8186fd39135
Akihiko Koizuka <koizuka@gmail.com>
2021-03-28 23:14:34 +0900

    なろうとなろうR18の結果をマージして更新時刻降順に

--- cmd/narou-react/src/App.tsx
+++ cmd/narou-react/src/App.tsx
@@ -2,8 +2,10 @@
 import useSWR, { SWRConfig } from 'swr';
 import { useEffect, useMemo } from 'react';
 import { Avatar, Badge, Box, Button, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
-import { DateTime } from 'luxon';
+import { DateTime, Duration } from 'luxon';
 import { Book } from '@material-ui/icons';
+
+const IgnoreDuration = Duration.fromObject({ days: 30 });
 
 type IsNoticeListRecord = {
   base_url: string;
@@ -19,6 +21,7 @@ type IsNoticeListItem = {
   bookmark: number;
   latest: number;
   title: string;
+  isR18: boolean;
 };
 
 function nextLink(item: IsNoticeListItem): string {
@@ -28,24 +31,38 @@ function nextLink(item: IsNoticeListItem): string {
   return `${item.base_url}${item.bookmark + 1}/`;
 }
 
-function openNextLink(item: IsNoticeListItem) {
-  window.open(nextLink(item), '_blank');
+function useIsNoticeList() {
+  const host = 'http://localhost:7676';
+
+  const { data: raw_items, error } = useSWR<IsNoticeListRecord[]>(`${host}/narou/isnoticelist`);
+  const { data: raw_items18, error: error18 } = useSWR<IsNoticeListRecord[]>(!error ? `${host}/r18/isnoticelist` : null);
+
+  const items: IsNoticeListItem[] | undefined = useMemo(
+    () => {
+      const tooOld = DateTime.now().minus(IgnoreDuration);
+
+      // なろう、R18のアイテムを混ぜて、古いアイテムを捨てて、更新日時降順にする
+      const n = [
+        ...(raw_items || []).map(i => ({ ...i, isR18: false })),
+        ...(raw_items18 || []).map(i => ({ ...i, isR18: true }))
+      ].map(i => ({ ...i, update_time: DateTime.fromISO(i.update_time) }))
+        .filter(i => i.update_time > tooOld)
+        .sort((a, b) =>
+          (a.update_time > b.update_time) ? -1 :
+            (a.update_time < b.update_time) ? 1 : 0
+        )
+      return n;
+    },
+    [raw_items, raw_items18]
+  );
+
+  return { data: error ? undefined : items, error: error || error18 };
 }
 
 function NarouUpdates() {
-  const { data: raw_items, error } = useSWR<IsNoticeListRecord[]>('http://localhost:7676/narou/isnoticelist')
-  const items: IsNoticeListItem[] | undefined = useMemo(
-    () => {
-      const n = raw_items?.map(i => ({ ...i, update_time: DateTime.fromISO(i.update_time) }))
-      return n;
-    },
-    [raw_items]
-  );
+  const { data: items, error } = useIsNoticeList();
 
-  const unreads = useMemo(
-    () => items?.filter(i => i.bookmark < i.latest),
-    [items]
-  );
+  const unreads = useMemo(() => items?.filter(i => i.bookmark < i.latest), [items]);
 
   useEffect(() => {
     document.title = `なろう 未読:${unreads?.length}`
@@ -57,14 +74,12 @@ function NarouUpdates() {
 
   return (
     <Box m={2} display="flex" flexDirection="column" bgcolor="background.paper">
-      {unreads?.length ?
-        <Button variant="contained" color="primary" onClick={() => openNextLink(unreads[0])}>
-          {`${unreads[0].title}(${unreads[0].bookmark + 1}部分)を開く`}
+      {unreads?.map((item, i) =>
+        <Button variant="contained" color={!i ? "primary" : undefined} href={nextLink(item)} target="_blank">
+          {`${item.title}(${item.bookmark + 1}部分)を開く`}
         </Button>
-        :
-        <></>
-      }
-      <p>{`未読: ${unreads?.length} items.`}</p>
+      )}
+      <p>{`未読: ${unreads?.length} 作品.`}</p>
       {items?.map(item =>
         <Box key={item.base_url} width="100%">
           <Button variant="outlined" href={nextLink(item)} target="_blank">
@@ -72,7 +87,7 @@ function NarouUpdates() {
               <ListItemAvatar>
                 <Badge color="primary" badgeContent={Math.max(item.latest - item.bookmark, 0)}>
                   <Avatar>
-                    <Book />
+                    <Book color={item.isR18 ? "secondary" : undefined} />
                   </Avatar>
                 </Badge>
               </ListItemAvatar>
