a24711ea9139fadb29fba2959e310d63bf5186ca
Akihiko Koizuka <koizuka@gmail.com>
2021-04-16 23:58:20 +0900

    ログイン失敗ダイアログ追加

--- narou-react/src/NarouLoginForm.tsx
+++ narou-react/src/NarouLoginForm.tsx
@@ -1,9 +1,10 @@
 import { useCallback, useRef, useState } from 'react';
-import { Box, Button, TextField } from '@material-ui/core';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@material-ui/core';
 
 export function NarouLoginForm(props: { server: string; onLogin: () => void; }) {
   const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState<string>('');
 
   const postLogin = useCallback(async () => {
     const formData = new FormData();
@@ -14,36 +15,57 @@ export function NarouLoginForm(props: { server: string; onLogin: () => void; }) 
       console.log(pair[0], pair[1]);
     }
 
-    const res = await fetch(`${props.server}/narou/login`, {
+    const res: Response = await fetch(`${props.server}/narou/login`, {
       method: 'POST',
       body: formData,
     });
-    const json = await res.json();
-    if (json) {
-      props.onLogin();
+    if (!res.ok) {
+      const text = await res.text();
+      setError(`${res.status} ${res.statusText}\n${text}`);
+    } else {
+      const json = await res.json();
+      if (json) {
+        props.onLogin();
+      }
     }
   }, [userId, password, props]);
 
   const passwordRef = useRef<HTMLInputElement>();
 
-  return (<form id="loginForm">
-    <h2>小説家になろうのログイン情報</h2>
-    <Box>
-      <TextField id="id" name="id" label="ID or email" autoFocus
-        value={userId} onChange={e => setUserId(e.target.value)}
-        onKeyPress={e => {
+  const closeError = useCallback(() => {
+    setError('');
+    passwordRef.current?.focus();
+  }, []);
+
+  return (<>
+    <Dialog open={error !== ''} onClose={() => closeError()}>
+      <DialogTitle>ログインできませんでした</DialogTitle>
+      <DialogContent>
+        <pre>{error}</pre>
+      </DialogContent>
+      <DialogActions>
+        <Button autoFocus onClick={() => closeError()}>OK</Button>
+      </DialogActions>
+    </Dialog>
+    <form id="loginForm">
+      <h2>小説家になろうのログイン情報</h2>
+      <Box>
+        <TextField id="id" name="id" label="ID or email" autoFocus
+          value={userId} onChange={e => setUserId(e.target.value)}
+          onKeyPress={e => {
+            if (e.key === 'Enter') {
+              passwordRef.current?.focus();
+            }
+          }}></TextField>
+      </Box>
+      <Box><TextField id="password" name="password" label="password" type="password"
+        value={password} onChange={e => setPassword(e.target.value)}
+        inputRef={passwordRef} onKeyPress={e => {
           if (e.key === 'Enter') {
-            passwordRef.current?.focus();
+            postLogin();
           }
-        }}></TextField>
-    </Box>
-    <Box><TextField id="password" name="password" label="password" type="password"
-      value={password} onChange={e => setPassword(e.target.value)}
-      inputRef={passwordRef} onKeyPress={e => {
-        if (e.key === 'Enter') {
-          postLogin();
-        }
-      }}></TextField></Box>
-    <Button onClick={postLogin}>login</Button>
-  </form>);
+        }}></TextField></Box>
+      <Button onClick={postLogin}>login</Button>
+    </form>
+  </>);
 }
