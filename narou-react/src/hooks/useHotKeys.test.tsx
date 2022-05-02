dd01cb4f185b089dafe63a218a50187719ca4bc0
Akihiko Koizuka <koizuka@gmail.com>
2022-05-02 13:12:57 +0900

    useHotKeys キー文字列を正規化

--- narou-react/src/hooks/useHotKeys.test.tsx
+++ narou-react/src/hooks/useHotKeys.test.tsx
@@ -6,11 +6,17 @@ test('checkKeyString', () => {
   expect(() => checkKeyString('ctrl+a')).not.toThrow();
   expect(() => checkKeyString('alt+a')).not.toThrow();
   expect(() => checkKeyString('meta+a')).not.toThrow();
+  expect(checkKeyString('a')).toBe('a');
+  expect(checkKeyString('A')).toBe('a');
+  expect(checkKeyString('Shift+a')).toBe('shift+a');
+  expect(checkKeyString('shift+A')).toBe('shift+a');
 
   expect(() => checkKeyString('+')).not.toThrow();
+  expect(checkKeyString('+')).toBe('+');
   expect(() => checkKeyString('shift++')).not.toThrow();
+  expect(checkKeyString('shift++')).toBe('shift++');
 
-  expect(() => checkKeyString('meta+shift+a')).toThrow();
+  expect(checkKeyString('meta+shift+a')).toBe('shift+meta+a');
   expect(() => checkKeyString('+a')).toThrow();
   expect(() => checkKeyString('shift++a')).toThrow();
   expect(() => checkKeyString('hoge+a')).toThrow();
--- narou-react/src/hooks/useHotKeys.tsx
+++ narou-react/src/hooks/useHotKeys.tsx
@@ -6,57 +6,46 @@ export type HotKeys = {
 
 type KeyCombination = {
   key: string;
-  shiftKey?: boolean;
-  ctrlKey?: boolean;
-  altKey?: boolean;
-  metaKey?: boolean;
+  shiftKey: boolean;
+  ctrlKey: boolean;
+  altKey: boolean;
+  metaKey: boolean;
 };
 
-function toKeyString(event: KeyCombination): string {
+function toKeyString(event: KeyboardEvent | KeyCombination): string {
   return [
-    event.shiftKey ? 'shift' : undefined,
-    event.ctrlKey ? 'ctrl' : undefined,
-    event.altKey ? 'alt' : undefined,
-    event.metaKey ? 'meta' : undefined,
-    event.key,
-  ].filter(s => s !== undefined).join('+')
+    event.shiftKey && 'shift',
+    event.ctrlKey && 'ctrl',
+    event.altKey && 'alt',
+    event.metaKey && 'meta',
+    event.key.toLowerCase(),
+  ].filter(s => s !== false).join('+');
 }
 
-export function checkKeyString(keyString: string) {
-  const [key, ...modifiers] =
-    keyString === '+'
-      ? ['+']
-      : keyString.endsWith('++')
-        ? ['+', ...keyString.slice(0, -2).split('+').reverse()]
-        : keyString.split('+').reverse();
-
-  const availableModifiers = ['shift', 'ctrl', 'alt', 'meta'];
-  if (modifiers.some(s => !availableModifiers.includes(s))) {
-    throw new Error(`HotKey(${keyString}): unknown modifiers: ${modifiers.filter(s => !availableModifiers.includes(s))}`);
+export function checkKeyString(keyString: string): string {
+  const match = keyString.toLowerCase().match(/^((?:(?:shift|ctrl|alt|meta)\+)*)(\+|\w+)$/);
+  if (match === null) {
+    throw new Error(`Invalid key string: ${keyString}`);
   }
+  const [_, modifiers, key] = match;
 
-  const combination: KeyCombination = {
+  return toKeyString({
     key,
-    shiftKey: modifiers.some(s => s === 'shift'),
-    ctrlKey: modifiers.some(s => s === 'ctrl'),
-    altKey: modifiers.some(s => s === 'alt'),
-    metaKey: modifiers.some(s => s === 'meta'),
-  };
-  const shouldBe = toKeyString(combination);
-
-  if (shouldBe !== keyString) {
-    throw new Error(`HotKey(${keyString}): invalid order: must be ${shouldBe} `);
-  }
+    shiftKey: /\bshift\b/.test(modifiers),
+    ctrlKey: /\bctrl\b/.test(modifiers),
+    altKey: /\balt\b/.test(modifiers),
+    metaKey: /\bmeta\b/.test(modifiers),
+  });
 }
 
 export function useHotKeys(hotkeys: HotKeys) {
   useEffect(() => {
     const keys = Object.keys(hotkeys);
     if (keys.length > 0) {
-      keys.forEach(key => checkKeyString(key));
+      const table = Object.fromEntries(keys.map(key => [checkKeyString(key), hotkeys[key]]));
 
       const onKeyDown = (event: KeyboardEvent) => {
-        const handler = hotkeys[toKeyString(event)];
+        const handler = table[toKeyString(event)];
         if (handler) {
           handler(event);
         }
