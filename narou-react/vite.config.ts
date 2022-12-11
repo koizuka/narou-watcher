da16864b964bfb5527aaa3ac61d361a0804a666c
Akihiko Koizuka <koizuka@gmail.com>
2022-12-11 18:15:55 +0900

    move some deps to dev and update vite react plugin

--- narou-react/vite.config.ts
+++ narou-react/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 import { DateTime } from 'luxon';
 
 export default defineConfig({
   build: {
     outDir: "build"
   },
-  plugins: [tsconfigPaths(), reactRefresh()],
+  plugins: [tsconfigPaths(), react()],
   define: {
     'process.env': process.env,
     'import.meta.env.BUILD_DATE': JSON.stringify(DateTime.now().toISO()),
