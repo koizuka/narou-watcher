c5b17ca5f79590abd289d9b38500e6dbe5010c6d
Akihiko Koizuka <koizuka@gmail.com>
2024-05-14 21:30:38 +0900

    remove web-vitals

--- narou-react/src/index.tsx
+++ narou-react/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -12,8 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
